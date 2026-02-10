1fbf2dc5bc3bdf6b07f5accd503eb8a0036f0013
GVG Global Group <gvgglobalgroupltd@gmail.com>
2026-02-10 09:11:20 -0400

    Revert: Remove all optimization logic and use straight image serving with global unoptimized=true

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,9 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default withBundleAnalyzer(nextConfig);
